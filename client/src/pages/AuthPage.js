e6ba162a977b0573da90ec3f46225e49fc188288
kostyakostya61 <kvoronin.exceedteam@gmail.com>
2020-10-26 12:04:10 +0300

    new title

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -41,7 +41,7 @@ export const AuthPage = () => {
     return (
         <div className="row">
             <div className="col s6 offset-s3">
-                <h1>ВЕБСАЙТ </h1>
+                <h1>Website</h1>
                 <div className="card blue darken-1">
                   <div className="card-content white-text">
                     <span className="card-title">Авторизация</span>
